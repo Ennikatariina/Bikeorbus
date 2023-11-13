59654405799671b07d0a2dae0e0e977a4454820c
Tomi Hänninen <101732080+hannisentomi@users.noreply.github.com>
2023-11-13 23:24:34 +0200

    lisättiin tyylejä
    Ja vaihdettiin buttonit pressableihin ainakin toistaiseksi.

--- components/Aloitus.js
+++ components/Aloitus.js
@@ -4,17 +4,29 @@ import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Header from './Header';
 import styles from '../style/styles';
+import { Pressable } from 'react-native';
 
 const Aloitus = () => {
   const navigation = useNavigation();
 
+
+  // Ekaan sivuun ei välttämättä tarvitse navbaria? Laitetaanko vasta kun kirjaudutaan?
   return (
-    <View>
+    <View style={styles.aloitus}>
       <Header />
       <Text style={styles.etusivuText}
       >Tervetuloa Bikeorbus sovellukseen!</Text>
-      <Button title="Create an account" onPress={() => navigation.navigate('LuoKayttaja')} />
-      <Button title="Log in" onPress={() => navigation.navigate('Kirjautuminen')} />
+      <Pressable style={styles.pressable}
+        onPress={() => navigation.navigate('LuoKayttaja')}>
+        <Text style={styles.pressableText}>Luo käyttäjä</Text>
+      </Pressable>
+
+      
+      <Pressable style={styles.pressable}
+        onPress={() => navigation.navigate('Kirjautuminen')}>
+        <Text style={styles.pressableText}>Kirjaudu</Text>
+      </Pressable>
+
     </View>
   );
 };
--- style/styles.js
+++ style/styles.js
@@ -1,6 +1,26 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+    aloitus: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      kirjautuminen: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      tulos: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      aloitus: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
     text: {
         fontSize: 16,
         color: 'black',
@@ -28,6 +48,31 @@ export default StyleSheet.create({
         color: 'black',
         fontWeight: 'bold',
         textAlign: 'center',
+      },
+      button: {
+        backgroundColor: 'blue',
+        color: 'white',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'blue',
+      },  
+      pressable: {
+        height: 60, 
+        width: 160,   
+        padding: 5,
+        margin: 10,
+        borderRadius: 5,
+        backgroundColor: 'teal',
+        borderWidth: 1,
+        borderColor: 'teal',
+        justifyContent: 'center',
+        alignItems: 'center',
+     
+      },
+      pressableText: {
+        color: 'white',  
+        fontSize: 16,  
+        fontWeight: 'bold',  
       },
 
 });
