aa9f4fe84b1df997f7c3d22b5a9888304de720e5
EnniKelloniemi <enni.kelloniemi@gmail.com>
2023-11-15 12:26:06 +0200

    firebase tiedosto ja kansio

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsTsk9m5SL84pj3YULqgo3vIi1haKfxD0",
+  authDomain: "bikeorbus-af868.firebaseapp.com",
+  projectId: "bikeorbus-af868",
+  storageBucket: "bikeorbus-af868.appspot.com",
+  messagingSenderId: "819698560843",
+  appId: "1:819698560843:web:d37ec2d6b05cc5e56a02ce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const USERS_REF ='/users/'
+
