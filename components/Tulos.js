d5139c0567412ea37af952b043bb218d1b5f8859
Tomi Hänninen <101732080+hannisentomi@users.noreply.github.com>
2023-11-17 19:53:07 +0200

    Lisättiin kelikameran haku

--- components/Tulos.js
+++ components/Tulos.js
@@ -1,42 +1,58 @@
-import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+
+import React, { useState, useEffect } from 'react';
+
+import { View, Text, Pressable, Image} from 'react-native';
+import axios from 'axios';
 import Header from './Header';
 import Footer from './Footer';
 import styles from '../style/styles';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator} from '@react-navigation/stack';
 
-
-
-
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-
-//Ei vielä vie mihinkään näppäimet
 export default Tulos = ({ navigation }) => {
-  return (
-   <View>
-      <Header />
-     <Text>Keli kuva</Text>
-     <Text>Säätiedot</Text>
-     <Text>Yhteenveto</Text>
+  const [weatherImage, setWeatherImage] = useState(null);
 
-     
-     <Pressable style={styles.pressable}
-     onPress={() => navigation.navigate('Pyoralla')}>
-        <Text style={styles.pressableText}
-        >Pyörällä</Text>
+
+
+  // Haetaan kelikuvan url. Tulevaisuudessa luodaan url haku funktio, joka hakee urlin sijainnnin perusteella. Presets[0] on kelikameroista paikan ensimmäinen ja se on ilmaistu oliona. 
+  useEffect(() => {
+    const fetchLatestWeatherImage = async () => {
+      try {
+        const response = await axios.get('https://tie.digitraffic.fi/api/weathercam/v1/stations/C12614');
+        const imageUrl = response.data.properties.presets[0].imageUrl;
+        if (response.data.properties.presets[0].imageUrl) {
+          setWeatherImage(response.data.properties.presets[0].imageUrl);
+          console.log('Viimeisin kelikuva: ', imageUrl);
+        } else {
+          console.log('Kelikuvaa ei saatavilla');
+        }
+      } catch (error) {
+        console.error('Virhe haettaessa kelikuvaa: ', error);
+      }
+    };
+
+    fetchLatestWeatherImage();
+  }, []); 
+
+  return (
+    <View>
+      <Header />
+      <Text>Keli kuva</Text>
+      <Text>Säätiedot</Text>
+      <Text>Yhteenveto</Text>
+      <Image source={{ uri: weatherImage }} style={{ width: 200, height: 200 }} />
+      
+      <Pressable style={styles.pressable} onPress={() => navigation.navigate('Pyoralla')}>
+        <Text style={styles.pressableText}>Pyörällä</Text>
       </Pressable>
-      <Pressable style={styles.pressable}
-       onPress={() => navigation.navigate('Bussilla')}>
-        <Text style={styles.pressableText}
-        >Bussilla</Text>
+      <Pressable style={styles.pressable} onPress={() => navigation.navigate('Bussilla')}>
+        <Text style={styles.pressableText}>Bussilla</Text>
       </Pressable>
     </View>
   );
 };
-
  
