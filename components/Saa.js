24d6ca7f2f8d966694c2ef42a965e96968e0c221
markov76 <101133846+markov76@users.noreply.github.com>
2023-11-25 21:23:02 +0200

    lisätty tuulia

--- components/Saa.js
+++ components/Saa.js
@@ -7,6 +7,8 @@ export default function Saa({latitude,longitude}) {
   const [temp,setTemp] = useState(0);
   const [description, setDescription] = useState('');
   const [icon, setIcon] = useState('');
+  const [wind, setWind] = useState(0);
+  const [suunta, setSuunta] = useState(0);
 
   useEffect(() => {
     const url = API_URL +
@@ -20,6 +22,8 @@ export default function Saa({latitude,longitude}) {
       (result) => { 
         setTemp(result.main.temp);
         setDescription(result.weather[0].description);
+        setWind(result.wind.speed);
+        setSuunta(result.wind.deg);
         setIcon(ICON_URL +  result.weather[0].icon + '@2x.png');
       },
       (error) => { 
@@ -33,6 +37,10 @@ export default function Saa({latitude,longitude}) {
     <>
       <Text style={styles.label}>Lämpötila</Text>
       <Text> {temp}</Text>
+      <Text style={styles.label}>Tuulen nopeus</Text>
+      <Text> {wind} </Text>
+      <Text style={styles.label}>Tuulen suunta</Text>
+      <Text> {suunta} </Text>
       <Text style={styles.label}>Description</Text>
       <Text>{description}</Text>
       <Image source={{uri: icon ? icon : null}} style={{width: 100,height: 100}}/>
