85d6a3a1dbceec2cf6279e4b859a671a7ccbda81
EnniKelloniemi <enni.kelloniemi@gmail.com>
2023-12-18 17:51:54 +0200

    switch enni12

---
+++ auth/deleteUser.js
@@ -0,0 +1,22 @@
+import {deleteUser as authDeleteUser, reauthenticateWithCredential, EmailAuthProvider  } from "firebase/auth";
+import { auth} from './authManager';
+import {db} from '../firebaseConfig'
+import { deleteDoc, collection, query, where, getDocs, doc } from "firebase/firestore";
+
+
+export default deleteUser = async ({ navigation, userConfirmed }) => {
+    const user = auth.currentUser;
+    try {
+      // Pyydä käyttäjää kirjautumaan sisään uudelleen
+      const credentials = EmailAuthProvider.credential(user.email, userConfirmed);
+      await reauthenticateWithCredential(user, credentials);
+
+      await authDeleteUser(user);
+      console.log('User deleted successfully.');
+  
+
+      navigation.navigate('Aloitus');
+    } catch (error) {
+      console.error('Error deleting user:', error.message);
+    }
+  };
